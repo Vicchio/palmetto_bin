510fb2f40990f9c1e0161686e979ea37143c1482
Vicchio <svicchi@g.clemson.edu>
2019-02-13 11:27:45 -0500

    sending over fill-template.py

---
+++ fill-template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# 
+# Stephen Patrick Vicchio 
+# 2019-02-13
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# 
+# Populates the INCAR and subvasp.sh files for job submission 
+# 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#
+#  INPUT: [file-template].ini  
+# OUTPUT: INCAR and pbs submission files for running the job  
+# 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+
+import sys
+import os
+from itertools import groupby
+import numpy as np
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# List of all functions
+
+def hello_world():
+	print('Hello World')
+	return 
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Main code program
+
+def main():
+	
+	hello_world()
+
+
+
+
+	return 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if __name__ == '__main__':
+	main()
+
