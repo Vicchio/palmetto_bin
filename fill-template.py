9c90b688938676c2a2a5131c800d725f08497fde
Vicchio <svicchi@g.clemson.edu>
2019-02-13 13:06:57 -0500

    adding more stuff to fill-template.py

--- fill-template.py
+++ fill-template.py
@@ -18,24 +18,130 @@ import sys
 import os
 from itertools import groupby
 import numpy as np
+import argparse 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # List of all functions
 
 def hello_world():
-	print('Hello World')
+	print('\nHello World\n')
 	return 
+
+def parsing_atoms(atom_list):
+	dict_atom = {}
+	dict_atom['list'] = atom_list
+	for atom in atom_list.split():
+		dict_atom[atom] = {}
+		dict_atom[atom]['C6'] = dispersion_values(atom)[0]
+		dict_atom[atom]['R0'] = dispersion_values(atom)[1] 	
+	
+	print(dict_atom)
+
+	return 
+
+def dispersion_values(atom):
+
+# The following values were taken from:
+# 'Semiempirical GGA-Type Density Functional Constructed with a Long-Range Dispersion Correction'
+# by Grimmes et al. (2006)
+
+	dict_dis_C6 = {'H' : 0.140,
+		       'He': 0.080,
+		       'Li': 1.610,
+                       'Be': 1.610,
+                       'B' : 3.130,
+		       'C' : 1.750,
+		       'N' : 1.230, 
+		       'O' : 0.700,
+		       'F' : 0.750,
+                       'Ne': 0.630,
+                       'Na': 5.710,
+                       'Mg': 5.710,
+		       'Al': 10.79,
+                       'Si': 9.230,
+		       'P' : 7.840,
+                       'S' : 5.570,
+   		       'Cl': 5.070, 
+		       'Ar': 4.610,
+                       'K' : 10.80,
+                       'Ca': 10.80,
+		       'Sc': 10.80,
+		       'Ti': 10.80,
+                       'V' : 10.80, 
+                       'Cr': 10.80, 
+                       'Mn': 10.80, 
+                       'Fe': 10.80, 
+                       'Co': 10.80, 
+                       'Ni': 10.80,
+                       'Cu': 10.80, 
+                       'Zn': 10.80,
+		       'Zr': 24.67} 
+	
+	dict_dis_R0 = {'H' : 1.001,
+		       'He': 1.012,
+		       'Li': 0.825,
+                       'Be': 1.408,
+                       'B' : 1.485,
+		       'C' : 1.452,
+		       'N' : 1.397, 
+		       'O' : 1.342,
+		       'F' : 1.287,
+                       'Ne': 1.243,
+                       'Na': 1.144,
+                       'Mg': 1.364,
+		       'Al': 1.639,
+                       'Si': 1.716,
+		       'P' : 1.705,
+                       'S' : 1.683,
+   		       'Cl': 1.639, 
+		       'Ar': 1.595,
+                       'K' : 1.485,
+                       'Ca': 1.474,
+		       'Sc': 1.562,
+		       'Ti': 1.562,
+                       'V' : 1.562, 
+                       'Cr': 1.562, 
+                       'Mn': 1.562, 
+                       'Fe': 1.562, 
+                       'Co': 1.562, 
+                       'Ni': 1.562,
+                       'Cu': 1.562, 
+                       'Zn': 1.562,
+		       'Zr': 1.639}  
+
+	if atom not in dict_dis_C6.keys():
+		print('ERROR')
+	else:
+		C6_val = dict_dis_C6[atom]
+		R0_val = dict_dis_R0[atom]
+
+	return C6_val, R0_val
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Main code program
 
 def main():
-	
+	# parsing the input arguments
+	parser = argparse.ArgumentParser(description="""\nThis script is
+		designed to fill premade template fills using .ini files
+		to create INCAR and PBS submission files.""")
+	parser.add_argument('-i', action='store', dest='ini_file',
+		help='.ini file to populate template')
+	parser.add_argument('-l', action='store', dest='potcar_atoms_order')
+	parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+
+	args = parser.parse_args()
+
+
+	# organizing the arguments 	
+	parsing_atoms(args.potcar_atoms_order)
+
+	# identifying the necessary template files 
 	hello_world()
 
 
-
+	# modifying the template file 
 
 	return 
 
