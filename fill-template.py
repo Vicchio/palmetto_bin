d7e0d137a3e29e33b217c5007f8abc011ef3b947
Stephen Vicchio <svicchi@g.clemson.edu>
2019-02-14 10:50:57 -0500

    adding the blank textfile to show the name

--- fill-template.py
+++ fill-template.py
@@ -60,7 +60,7 @@ def string_generation_dis(dict_):
     list_order = dict_[KEY_LIST]
     
     print(list_order.split())
-    for atom in list_order.split():
+    for atom in (list_order.split()):
         print(atom)
         print(str(dict_[atom][KEY_C6]))
         print(str(dict_[atom][KEY_R0]))
