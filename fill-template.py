7219bfbcf96b28e1b07a5771845a74a0935d0947
Stephen Vicchio <svicchi@g.clemson.edu>
2019-02-14 15:58:44 -0500

    please work

--- fill-template.py
+++ fill-template.py
@@ -85,11 +85,11 @@ def filling_strings(string, key_, status=False):
     if key_ == [KEY_C6] and status is False:
         if len(string.split('.')[0]) == 2:
             status = True 
-            string_out = string
+            string_out = string.ljust(2)
             print('I made it here!')
         elif len(string.split('.')[0]) == 1:
             status = False
-            string_out = string
+            string_out = ' ' + str(string.ljust(2))
         else:
             raise ValueError('\n\n Issue with DFT2 Parameters\n')
     
