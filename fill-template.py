ed4b61fd1bebedcab5c968ea139f1fac9a95c381
Stephen Vicchio <svicchi@g.clemson.edu>
2019-02-14 09:29:46 -0500

    made a small change

--- fill-template.py
+++ fill-template.py
@@ -3,29 +3,29 @@
 # Stephen Patrick Vicchio 
 # 2019-02-13
 #
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 
 # Populates the INCAR and subvasp.sh files for job submission 
 # 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
 #  INPUT: [file-template].ini  
 # OUTPUT: INCAR and pbs submission files for running the job  
 # 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 import sys
 import os
 import argparse 
 from shutil import copy
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 TEMPLATE_DIR = '/common/curium/svicchi/zy-templates/00-MOF'
 PBS_SUB_DIR  = '/common/curium/svicchi/zy-templates'
 
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # List of all functions
 
 def hello_world():
@@ -33,6 +33,8 @@ def hello_world():
 	return 
 
 def parsing_atoms(atom_list):
+
+    
 	dict_atom = {}
 	dict_atom['list'] = atom_list
 	for atom in atom_list.split():
@@ -51,74 +53,79 @@ def populating_submission_file(name):
 
 
 def dispersion_values(atom):
+    """
+    The following values were taken from: 'Semiempirical GGA-Type Density 
+    Functional Constructed with a Long-Range Dispersion Correction' by Grommes 
+    et al. (2006). 
 
-# The following values were taken from:
-# 'Semiempirical GGA-Type Density Functional Constructed with a Long-Range Dispersion Correction'
-# by Grimmes et al. (2006)
+    Function: Returns the DFT2 parameters to include dispersion in calculation
+    Input: atom string (i.e. C)
+    Output: The C6 and R0 parameters for DFT2
+    """
 
 	dict_dis_C6 = {'H' : 0.140,
-		       'He': 0.080,
-		       'Li': 1.610,
-                       'Be': 1.610,
-                       'B' : 3.130,
-		       'C' : 1.750,
-		       'N' : 1.230, 
-		       'O' : 0.700,
-		       'F' : 0.750,
-                       'Ne': 0.630,
-                       'Na': 5.710,
-                       'Mg': 5.710,
-		       'Al': 10.79,
-                       'Si': 9.230,
-		       'P' : 7.840,
-                       'S' : 5.570,
-   		       'Cl': 5.070, 
-		       'Ar': 4.610,
-                       'K' : 10.80,
-                       'Ca': 10.80,
-		       'Sc': 10.80,
-		       'Ti': 10.80,
-                       'V' : 10.80, 
-                       'Cr': 10.80, 
-                       'Mn': 10.80, 
-                       'Fe': 10.80, 
-                       'Co': 10.80, 
-                       'Ni': 10.80,
-                       'Cu': 10.80, 
-                       'Zn': 10.80,
-		       'Zr': 24.67} 
+                'He': 0.080,
+                'Li': 1.610,
+                'Be': 1.610,
+                'B' : 3.130,
+                'C' : 1.750,
+                'N' : 1.230,
+                'O' : 0.700,
+                'F' : 0.750,
+                'Ne': 0.630,
+                'Na': 5.710,
+                'Mg': 5.710,
+                'Al': 10.79,
+                'Si': 9.230,
+                'P' : 7.840,
+                'S' : 5.570,
+                'Cl': 5.070,
+                'Ar': 4.610,
+                'K' : 10.80,
+                'Ca': 10.80,
+                'Sc': 10.80,
+                'Ti': 10.80,
+                'V' : 10.80, 
+                'Cr': 10.80, 
+                'Mn': 10.80, 
+                'Fe': 10.80, 
+                'Co': 10.80, 
+                'Ni': 10.80,
+                'Cu': 10.80, 
+                'Zn': 10.80,
+                'Zr': 24.67} 
 	
 	dict_dis_R0 = {'H' : 1.001,
 		       'He': 1.012,
 		       'Li': 0.825,
-                       'Be': 1.408,
-                       'B' : 1.485,
+               'Be': 1.408,
+               'B' : 1.485,
 		       'C' : 1.452,
-		       'N' : 1.397, 
+               'N' : 1.397,
 		       'O' : 1.342,
 		       'F' : 1.287,
-                       'Ne': 1.243,
-                       'Na': 1.144,
-                       'Mg': 1.364,
+               'Ne': 1.243,
+               'Na': 1.144,
+               'Mg': 1.364,
 		       'Al': 1.639,
-                       'Si': 1.716,
-		       'P' : 1.705,
-                       'S' : 1.683,
-   		       'Cl': 1.639, 
-		       'Ar': 1.595,
-                       'K' : 1.485,
-                       'Ca': 1.474,
-		       'Sc': 1.562,
-		       'Ti': 1.562,
-                       'V' : 1.562, 
-                       'Cr': 1.562, 
-                       'Mn': 1.562, 
-                       'Fe': 1.562, 
-                       'Co': 1.562, 
-                       'Ni': 1.562,
-                       'Cu': 1.562, 
-                       'Zn': 1.562,
-		       'Zr': 1.639}  
+               'Si': 1.716,
+               'P' : 1.705,
+               'S' : 1.683,
+               'Cl': 1.639,
+               'Ar': 1.595,
+               'K' : 1.485,
+               'Ca': 1.474,
+               'Sc': 1.562,
+               'Ti': 1.562,
+               'V' : 1.562,
+               'Cr': 1.562,
+               'Mn': 1.562,
+               'Fe': 1.562,
+               'Co': 1.562,
+               'Ni': 1.562,
+               'Cu': 1.562,
+               'Zn': 1.562,
+               'Zr': 1.639}  
 
 	if atom not in dict_dis_C6.keys():
 		raise ValueError('\n\nAtom Dispersion NOT listed!\n')
@@ -129,7 +136,7 @@ def dispersion_values(atom):
 	return C6_val, R0_val
 
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Main code program
 
 def main():
@@ -150,8 +157,10 @@ def main():
 
 	# identifying the necessary template files 
 	hello_world()
-	copy(os.path.join(PBS_SUB_DIR, 'template_subvasp.sh'), os.path.join(os.getcwd(), 'subvasp.sh'))
-	copy(os.path.join(PBS_SUB_DIR, 'template_subvasp.sh'), os.path.join(os.getcwd(), 'subvasp.sh'))
+	copy(os.path.join(PBS_SUB_DIR, 'template_subvasp.sh'), os.path.join(
+            os.getcwd(), 'subvasp.sh'))
+	copy(os.path.join(PBS_SUB_DIR, 'template_subvasp.sh'), os.path.join(
+            os.getcwd(), 'subvasp.sh'))
 	
 	# modifying the template file 
 
