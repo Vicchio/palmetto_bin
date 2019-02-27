d46f9144dbef0263073f799e40329126ef0395ad
Stephen Vicchio <svicchi@g.clemson.edu>
2019-02-27 12:46:29 -0500

    sending

--- parser-OUTCAR.py
+++ parser-OUTCAR.py
@@ -233,7 +233,7 @@ def main():
                 plt.title('Convergence results for ' + str(electronic_) + ' step')
                 plt.ylim((math.log10(float(10e-8)),math.log10(float(10e5))))
                 plt.scatter(electronic_dict[electronic_][SCF_KEY], electronic_dict[electronic_][DIFF_KEY])
-                plt.plot(electronic_dict[electronic_][SCF_KEY], np.array((len(electronic_dict[electronic_][SCF_KEY], EDIFF))))
+                plt.plot(electronic_dict[electronic_][SCF_KEY], np.array((len(electronic_dict[electronic_][SCF_KEY])), EDIFF))
                 plt.xlabel('SCF Iteration #')
                 plt.ylabel('Log|dE|')            
                 plt.savefig(os.path.join(working_dir, filename) + '.png')
