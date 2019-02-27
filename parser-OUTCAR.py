f0945438cbe7785b4d80b9031a0a7b49a52595ba
Stephen Vicchio <svicchi@g.clemson.edu>
2019-02-27 11:03:40 -0500

    sending

--- parser-OUTCAR.py
+++ parser-OUTCAR.py
@@ -194,14 +194,14 @@ def main():
             # writing the OUTPUTS
             if electronic_status is False: 
                 print(electronic_count, scf_count)
-                stepstr = str(electronic_count).rjust(4)
-#    				energystr = "Energy: " + ("%3.6f" % (energy)).rjust(12)
-#    				logdestr = "Log|dE|: " + ("%1.3f" % (dE)).rjust(6)					
-                iterstr = "SCF: " + ("%3i" % (scf_count))
-                avgfstr="Avg|F|: " + ("%2.3f" % (average_force)).rjust(6)
-                maxfstr="Max|F|: " + ("%2.3f" % (max_force)).rjust(6)
-                timestr="Time: " + ("%3.2fm" % (cputime_min)).rjust(6)
-#                 
+#                stepstr = str(electronic_count).rjust(4)
+##    				energystr = "Energy: " + ("%3.6f" % (energy)).rjust(12)
+##    				logdestr = "Log|dE|: " + ("%1.3f" % (dE)).rjust(6)					
+#                iterstr = "SCF: " + ("%3i" % (scf_count))
+#                avgfstr="Avg|F|: " + ("%2.3f" % (average_force)).rjust(6)
+#                maxfstr="Max|F|: " + ("%2.3f" % (max_force)).rjust(6)
+#                timestr="Time: " + ("%3.2fm" % (cputime_min)).rjust(6)
+##                 
 #                    
 #                print(stepstr, iterstr, avgfstr, maxfstr, timestr)
 #                
