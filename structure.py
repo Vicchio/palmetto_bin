d1cd51958777deac36f7512be64f17829d287db6
Stephen Vicchio <svicchi@g.clemson.edu>
2019-02-27 16:49:13 -0500

    sending

--- structure.py
+++ structure.py
@@ -77,8 +77,7 @@ def main():
         
         SEARCH_='Direct'
         
-        coordinate_line = str(subprocess.check_output(['grep', '-n', SEARCH_, POSCARfile])).replace(':'+SEARCH_,'')
-        coordinate_line = coordinate_line.strip('\n')
+        coordinate_line = str(subprocess.check_output(['grep', '-n', SEARCH_, POSCARfile])).split(':')[0]
         
         print(coordinate_line, type(coordinate_line))
         
