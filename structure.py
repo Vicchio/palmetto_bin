aa77e967dae6de55e5c215c3277cca757a2e7b6d
Stephen Vicchio <svicchi@g.clemson.edu>
2019-02-27 19:24:53 -0500

    sending

--- structure.py
+++ structure.py
@@ -125,7 +125,7 @@ def main():
                             count += 1  
                     elif line == coordinate_line-1:
                         atom_list = list_of_atoms(coordinate_line, atoms_dict)
-                        new_files_lines.append(atom_list[0:coordinate_line-1])
+                        new_file_lines.append(atom_list[0:coordinate_line-1])
                         
                         print(new_file_lines)
                         
