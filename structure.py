753cdcaa6cad0c9b146ee172241184d31eeaa0bf
Stephen Vicchio <svicchi@g.clemson.edu>
2019-02-28 13:34:18 -0500

    sending

--- structure.py
+++ structure.py
@@ -286,8 +286,15 @@ def main():
         # Writing the new POSCAR file for the frozen and unfrozen atoms: 
         with open(os.path.join(os.getcwd(), 'relax-POSCAR.txt'), 'w') as RELAX_POSCAR, \
         open(os.path.join(os.getcwd(), 'modified-POSCAR.txt'), 'w') as FREEZE_POSCAR:
-            for aline in MODPOSCARlines:    
-                print(aline)
+            for aline in MODPOSCARlines:
+                if aline <= coordinate_line:
+                    print(MODPOSCARlines[aline])
+                else:
+                    if aline.split()[0] in list_atoms_freeze:
+                        print('Freeze atom')
+                    elif aline.split()[0] in list_atoms_relax:
+                        print('Relax atom')
+                
             
                 
 #                             FREEZE_POSCAR.write(MODPOSCARlines[mline])
