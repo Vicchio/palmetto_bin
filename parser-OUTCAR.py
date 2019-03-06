9c6a19cb794c4c79570cfdec7797eac3d7d25960
Stephen Vicchio <svicchi@g.clemson.edu>
2019-03-05 20:09:48 -0500

    making a few changes to the script

--- parser-OUTCAR.py
+++ parser-OUTCAR.py
@@ -79,8 +79,7 @@ def main():
     
     if os.path.isfile(args.OUTCAR_file) is True: 
         READFILE = args.OUTCAR_file
-        print(os.path.isfile(os.path.join(os.getcwd, 'OUTCAR')))
-        print(os.path.join(os.getcwd, 'OUTCAR'))
+        print(os.path.isfile('OUTCAR'))
     elif os.path.isfile(os.path.join(os.getcwd, 'OUTCAR')) is True:
         READFILE = os.path.join(os.getcwd, 'OUTCAR')
     
