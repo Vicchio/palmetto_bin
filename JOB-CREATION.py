bc2f4224126bc51ec0c255c30cddba842197ea08
Stephen Vicchio <svicchi@g.clemson.edu>
2019-03-18 14:19:01 -0400

    sending

--- JOB-CREATION.py
+++ JOB-CREATION.py
@@ -44,6 +44,8 @@ def main():
                                      contain the KPOINTS, WAVECAR, INCAR, and
                                      POTCAR to continue VASP calculation runs.
                                      \n""")
+    parser.add_argument('-i', action='store', dest='START_DIR', default=STAGE1, 
+                        help='The name of the first directory to start job from.')
 #    parser.add_argument('-i', action='store', dest='POSCAR_FILE', default=None,
 #                        help='POSCAR file to read')
 #    parser.add_argument('-p', action='store', dest='POTCAR_FILE', default=None,
@@ -58,16 +60,25 @@ def main():
 #        sys.stderr.write(ENDC+"\n")
 #        sys.exit()
     
-    if os.path.isdir(os.path.join(DIR_, STAGE1)) is True: 
-        stage1_dir = os.path.join(DIR_, STAGE1)
+    if os.path.isdir(os.path.join(DIR_, args.START_DIR)) is True: 
+        stage1_dir = os.path.join(DIR_, args.START_DIR)
         status_POTCAR  = os.path.isfile(os.path.join(stage1_dir, POTCAR))
         status_KPOINTS = os.path.isfile(os.path.join(stage1_dir, KPOINTS))
         status_WAVECAR = os.path.isfile(os.path.join(stage1_dir, WAVECAR))
-        status_CONTCAR = os.path.isfile(os.path.join(stage1_dir, CONTCAR))
+        status_CONTCAR = os.path.isfile(os.path.join(stage1_dir, CONTCAR)) 
+        if status_POTCAR is False or status_KPOINTS is False or status_WAVECAR is False or status_CONTCAR is False: 
+            sys.stderr.write(FAIL)
+            sys.stderr.write("\nOne of the big four files is missing from " + str(args.START_DIR) + "!\n")
+            sys.stderr.write(ENDC+"\n")
+            sys.exit()
+        else: 
+            stage1_POTCAR  = os.path.join(stage1_dir, POTCAR)
+            stage1_KPOINTS = os.path.join(stage1_dir, KPOINTS)
+            stage1_WAVECAR = os.path.join(stage1_dir, WAVECAR)
+            stage1_CONTCAR = os.path.join(stage1_dir, CONTCAR)
+            
         
-        
-        print(status_POTCAR, status_KPOINTS, status_WAVECAR, status_CONTCAR)
-        
+
     else: 
         sys.stderr.write(FAIL)
         sys.stderr.write("\nThe 00-1st-stage directory doesn't exist.\n")
