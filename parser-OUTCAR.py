2ea01129ad86cc8bf0b20b0de52f4a1e7d830268
Stephen Vicchio <svicchi@g.clemson.edu>
2019-03-20 16:07:34 -0400

    sending

--- parser-OUTCAR.py
+++ parser-OUTCAR.py
@@ -317,31 +317,30 @@ def main():
             avgfstr   = "Avg|F|: " + ("%2.3f" % (force_dict[previous_electronic_step][AVERAGE_FORCE])).rjust(6)
             volstr    = "Vol.: " + ("%3.1f" % (volume_val)).rjust(5)
             maxfstr   = "Max|F|: " + ("%2.3f" % (force_dict[previous_electronic_step][MAX_FORCE])).rjust(6)
+            atomstr   = "Atom: " + str(force_dict[previous_electronic_step][MAX_ATOM]).rjust(6)
             timehrstr   = "Time: " + ("%3.2fhr" % (time_dict[previous_electronic_step]['hours'])).rjust(6)
             if args.STOP_DISPLAY is True:                
                 if spinpolarized is True:
                     magstr="Mag: " + ("%2.2f" % (magmom)).rjust(6)
-                    parser_file_write.write(stepstr + ' ' + energystr + ' ' + logdestr + ' ' + iterstr + ' ' + avgfstr + ' ' + maxfstr + ' ' + volstr + ' ' + magstr + ' ' + timehrstr+ '\n')
+                    parser_file_write.write(stepstr + ' ' + energystr + ' ' + logdestr + ' ' + iterstr + ' ' + avgfstr + ' ' + maxfstr + ' ' + atomstr + ' ' + volstr + ' ' + magstr + ' ' + timehrstr+ '\n')
                     parser_file_write.write('\n')
                 else:
-                    parser_file_write.write(str(stepstr + energystr + logdestr + iterstr + avgfstr + maxfstr + volstr + timehrstr) + '\n')
+                    parser_file_write.write(str(stepstr + energystr + logdestr + iterstr + avgfstr + maxfstr + atomstr + volstr + timehrstr) + '\n')
                     parser_file_write.write('\n')
             else: 
                 if spinpolarized is True:
                     magstr="Mag: " + ("%2.2f" % (magmom)).rjust(6)
-                    print(stepstr, energystr, logdestr, iterstr, avgfstr, maxfstr, volstr, magstr, timehrstr)
+                    print(stepstr, energystr, logdestr, iterstr, avgfstr, maxfstr, atomstr, volstr, magstr, timehrstr)
                     print('')
-                    parser_file_write.write(stepstr + ' ' + energystr + ' ' + logdestr + ' ' + iterstr + ' ' + avgfstr + ' ' + maxfstr + ' ' + volstr + ' ' + magstr + ' ' + timehrstr+ '\n')
+                    parser_file_write.write(stepstr + ' ' + energystr + ' ' + logdestr + ' ' + iterstr + ' ' + avgfstr + ' ' + maxfstr + ' ' + atomstr + ' ' + volstr + ' ' + magstr + ' ' + timehrstr+ '\n')
                     parser_file_write.write('\n')
                 else:
-                    print(stepstr, energystr, logdestr, iterstr, avgfstr, maxfstr, volstr, timehrstr)   
+                    print(stepstr, energystr, logdestr, iterstr, avgfstr, maxfstr, atomstr, volstr, timehrstr)       
                     print('')
-                    parser_file_write.write(str(stepstr + energystr + logdestr + iterstr + avgfstr + maxfstr + volstr + timehrstr) + '\n') 
+                    parser_file_write.write(str(stepstr + energystr + logdestr + iterstr + avgfstr + maxfstr + atomstr + volstr + timehrstr) + '\n') 
                     parser_file_write.write('\n')
                 
         parser_file_write.close()
-        
-        
         
     # CREATING THE PLOTS THAT SHOW THE CONVERGENCE CRITERIA 
     if args.OUTPUT_SCF is True: 
