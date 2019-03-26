8ca29b608e298cd7c985425127f522d7fb0935a5
Stephen Vicchio <svicchi@g.clemson.edu>
2019-03-26 17:18:22 -0400

    sending

--- parser-OUTCAR.py
+++ parser-OUTCAR.py
@@ -314,7 +314,7 @@ def main():
                 
             # Computes the cell volume for each step
             if re_volume.search(line):
-                print(line)
+                print(line.split()[4])
                 volume_val[electronic_count] = float(line.split()[4])
                 if volume_val is None: 
                     volume_val = float(line.split()[4])
