ad3de5d6fec7a529481daec3d9a67dc429b37b3f
Stephen Vicchio <svicchi@g.clemson.edu>
2019-03-27 13:08:24 -0400

    sending

--- parser-OUTCAR.py
+++ parser-OUTCAR.py
@@ -402,7 +402,7 @@ def main():
     elif FINISH_RUN_STATUS is False: 
         print('\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n')  
         for step in electronic_dict.keys():
-            print('I AM HERE!')
+            print(step)
             if step < len(electronic_dict.keys()):
                 stepstr   = str(str(step).zfill(2)).rjust(5)
                 energystr = "Energy: " + ("%3.6f" % (electronic_dict[step][ENERGY_KEY][-1])).rjust(12)
