3ecb2129c2cd147b7d839f2035f6273e4b9f7ba1
Stephen Vicchio <svicchi@g.clemson.edu>
2019-04-10 17:22:08 -0400

    sending

--- structure.py
+++ structure.py
@@ -225,7 +225,7 @@ def main():
                         atoms_dict[atom_add] = int(POSCARlines[line].split()[count])
                         count += 1
                     atom_list = poscar_atom_string(atoms_dict)
-                MOD_POSCAR.write(line)
+                MOD_POSCAR.write(POSCARlines[line])
                 
 #TODO: make the atoms dict to read all of the atoms string!     
             
