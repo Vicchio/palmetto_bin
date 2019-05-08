a9936ea02f2b90e446335553485f37515fc4c205
Stephen Vicchio <svicchi@g.clemson.edu>
2019-05-08 11:17:15 -0400

    sending

--- structure.py
+++ structure.py
@@ -429,21 +429,20 @@ def main():
                         RELAX_POSCAR.write(''.join(relax_string))
                         RELAX_POSCAR.write(''.join(relax_string_num))
                         
-                            freeze_string = []
-                            freeze_string_num = []
-                            for key_f in dict_freeze.keys():
-                                if dict_freeze[key_f] != 0: 
-                                    freeze_string.append(str(key_f).rjust(4))
-                                    freeze_string_num.append(str(dict_freeze[key_f]).rjust(4))
-                            freeze_string.append('\n')
-                            freeze_string_num.append('\n')
-                            FREEZE_POSCAR.write(''.join(freeze_string))
-                            FREEZE_POSCAR.write(''.join(freeze_string_num))
-                            UPDATED_POSCAR.write(MODPOSCARlines[aline][10:])
+                        freeze_string = []
+                        freeze_string_num = []
+                        for key_f in dict_freeze.keys():
+                            if dict_freeze[key_f] != 0: 
+                                freeze_string.append(str(key_f).rjust(4))
+                                freeze_string_num.append(str(dict_freeze[key_f]).rjust(4))
+                        freeze_string.append('\n')
+                        freeze_string_num.append('\n')
+                        FREEZE_POSCAR.write(''.join(freeze_string))
+                        FREEZE_POSCAR.write(''.join(freeze_string_num))
+                        UPDATED_POSCAR.write(MODPOSCARlines[aline][10:])
                             
                             
                 for aline in range(coordinate_line, coordinate_line + len(atom_list)):
-                    print(aline)
                     xcstr_write = str(MODPOSCARlines[aline].split()[0]).rjust(19)
                     ycstr_write = str(MODPOSCARlines[aline].split()[1]).rjust(20)
                     zcstr_write = str(MODPOSCARlines[aline].split()[2]).rjust(20)
