0425191c9e55941e72264f2e6f27a72663913d89
Stephen Vicchio <svicchi@g.clemson.edu>
2019-06-19 11:26:53 -0400

    sending ase-structure

--- ase-structure.py
+++ ase-structure.py
@@ -45,6 +45,7 @@ def main():
     structure = ase.Atoms(ase.io.read(args.INPUT_FILE, format=args.TYPE_FILE))
     
     print(structure.pbc)
+    print(structure.get_chemical_symbols())
 
 #    print(structure.get_positions)
 #    print(build.niggli_reduce(structure))                
