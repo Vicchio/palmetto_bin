b941f5453699d531c913ee6bd1afae5492e446ec
Stephen Vicchio <svicchi@g.clemson.edu>
2019-09-09 18:18:56 -0400

    sending

--- convert-structure.py
+++ convert-structure.py
@@ -40,7 +40,7 @@ args = parser.parse_args()
 filename=args.INPUT_FILE.split('.')[0]
 structure = read(args.INPUT_FILE,format=args.INPUT_FILE_TYPE)
 sorted_structure = sort(structure)
-write(filename + args.OUTPUT_FILE,sorted_structure,format=args.OUTPUT_FILE)
+write(filename + '.' + args.OUTPUT_FILE,sorted_structure,format=args.OUTPUT_FILE)
 
 # To create for ASE not including the list of atoms after line 5... 
 
