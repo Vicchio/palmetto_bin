486f8f078ef9f27f26953475f33b06d1d4f94d29
Stephen Vicchio <svicchi@g.clemson.edu>
2019-10-04 17:07:34 -0400

    hi mom

--- cp2k-cleanup.py
+++ cp2k-cleanup.py
@@ -103,6 +103,7 @@ def main():
             if COORD_FINISH_STATUS == False and COORD_COUNT_STATUS == True:
                 line_info = line.split()
                 if len(line.split()) != 2:
+                    print(line)
                     if line_info[0] not in atom_dict.keys():
                         atom_dict[str(line_info[0])] = 0 
                     atom_dict[str(line_info[0])] += 1 
