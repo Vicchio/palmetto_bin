a2d6dd44523761cfaea5015b965d3b278d3b398a
Stephen Vicchio <svicchi@g.clemson.edu>
2019-10-04 22:14:40 -0400

    hi mom

--- cp2k-cleanup.py
+++ cp2k-cleanup.py
@@ -127,7 +127,7 @@ def main():
                 
             line_count += 1
             if print_string is not None and print_string_status is True : 
-                CLEAN_INPUT.write(print_string)
+                CLEAN_INPUT.write(print_string + '\n')
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # R U N N I N G   S C R I P T 
     
